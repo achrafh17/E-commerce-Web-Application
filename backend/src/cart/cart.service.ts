66ee75b582c6392270bbb7a00a510c6f5401aeb7
Achraf <achrafhafid565@gmail.com>
2025-11-08 21:31:43 +0100

    create update on cartService

--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -13,11 +13,21 @@ export class CartService {
     const cart = await this.prisma.cart.create({ data: { ...data, userId } });
     return cart;
   }
+  async update(data: createCartDto, userId: string, id: string) {
+    if (!isNaN(parseInt(userId)))
+      throw new BadRequestException('ID format not found');
+    if (!isNaN(parseInt(id)))
+      throw new BadRequestException('ID format not found');
+    const cartCheck = await this.prisma.cart.update({
+      where: { id: parseInt(id) },
+      data: data,
+    });
+  }
   async delete(id: string) {
     if (!isNaN(parseInt(id)))
       throw new BadRequestException('ID format not found');
     const cartCheck = await this.prisma.cart.findUnique({
-      where: { id: parseInt },
+      where: { id: parseInt(id) },
     });
     if (!cartCheck) throw new NotFoundException('cart not found');
     await this.prisma.cart.delete({ where: { id: parseInt(id) } });
