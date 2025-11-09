96d1e0cf0f272b2761d47d91c366ce430e87ebcd
Achraf <achrafhafid565@gmail.com>
2025-11-09 14:56:35 +0100

    add create Cart-Item function on cart-item service

--- backend/src/cart-item/cart-item.service.ts
+++ backend/src/cart-item/cart-item.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { createCartItemDto } from './dto/create-cartItem.dto';
 
 @Injectable()
-export class CartItemService {}
+export class CartItemService {
+  constructor(private readonly primsa: PrismaService) {}
+  async create(data: createCartItemDto) {
+    const cartItem = await this.primsa.cartItem.create(data);
+    return cartItem;
+  }
+}
