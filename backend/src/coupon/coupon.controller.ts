2d37af1836186537845fbcfc1325dc9ea21a4649
Achraf <achrafhafid565@gmail.com>
2025-11-14 00:17:39 +0100

    add getUserLogs and getAll on logService and logsController

--- backend/src/coupon/coupon.controller.ts
+++ backend/src/coupon/coupon.controller.ts
@@ -2,14 +2,17 @@ import { Controller, Post, UseGuards, Req, Body } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { createCouponDto } from './dto/create-coupon.dto';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('coupons')
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
+  @Roles('admin')
   @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() data: createCouponDto, @Req() req) {
     const coupon = await this.couponService.create(data, req.user.id);
     return coupon;
   }
+
 }
